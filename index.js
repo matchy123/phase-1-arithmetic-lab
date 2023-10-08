cce88e2f551100fbf1be44608e677526b89b90a6
matchy123 <dikomohamed287@gmail.com>
2023-10-09 00:53:08 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+// variable = var 1 * var 2
+var num1 = 31;
+var num2 = 2;
+let multiply = (num1 * num2);
+console.log(multiply);
+
+
+//Math.random(); generating random numbers but greater than 0
+const random = Math.floor(Math.random() *65)+1;
+console.log(random);
+
+//getting the remainder after dividing...
+var num3 = 24
+var num4 = 5
+let mod = (num3 % num4);
+console.log(mod);
+
+//obtaining the maximum number in a set.
+let max = Math.max(4,5, 17, 5, 14, 20);
+console.log(max);
